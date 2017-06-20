e5ed1304cf903f5aade16060858b0eba6a329497
rjchnel <kf135117@gmail.com>
2017-06-20 21:41:04 +0800

    tow page ok

--- app/src/main/java/com/example/user/newcoffeepuzzle/MainActivity.java
+++ app/src/main/java/com/example/user/newcoffeepuzzle/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage_layout);
 
-        //隱藏專案名稱  方法二  test for one  test three
+        //隱藏專案名稱  方法二  test for one  test threeddd
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
