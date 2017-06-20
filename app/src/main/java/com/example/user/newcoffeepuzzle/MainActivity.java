13f77c2f77847e37bdfd618a298b42aa86b6f38a
rjchnel <kf135117@gmail.com>
2017-06-20 20:26:51 +0800

    sec ommit

--- app/src/main/java/com/example/user/newcoffeepuzzle/MainActivity.java
+++ app/src/main/java/com/example/user/newcoffeepuzzle/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage_layout);
 
-        //隱藏專案名稱  方法二
+        //隱藏專案名稱  方法二  test for one
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
