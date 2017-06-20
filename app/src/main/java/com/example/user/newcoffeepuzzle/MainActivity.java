57fc0b240507a60363fb6e9976b0aafe1d4e2060
rjchnel <kf135117@gmail.com>
2017-06-20 20:24:59 +0800

    first commit

---
+++ app/src/main/java/com/example/user/newcoffeepuzzle/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.user.newcoffeepuzzle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homepage_layout);
+
+        //隱藏專案名稱  方法二
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        setContentView(R.layout.homepage_layout);
+
+
+    }
+}
