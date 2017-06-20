33c7613383f4e7e38bea0657c7c851731cdee3e8
rjchnel <kf135117@gmail.com>
2017-06-20 23:13:33 +0800

    skip to maps ok but no navigation

--- app/src/main/java/com/example/user/newcoffeepuzzle/LoginActivity.java
+++ app/src/main/java/com/example/user/newcoffeepuzzle/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.newcoffeepuzzle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -29,7 +30,8 @@ public class LoginActivity extends AppCompatActivity {
 
 
     public void onbtLoginClick(View view) {
-
+        Intent intent = new Intent(this,MapsActivity.class);
+        startActivity(intent);
 
     }
 
