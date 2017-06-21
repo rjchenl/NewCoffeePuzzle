217399c0a777f05bc29191fdfed56bf2d9b372a3
Java <c973978549@gmail.com>
2017-06-21 15:27:12 +0800

    home ok

--- app/src/main/java/com/example/user/newcoffeepuzzle/MainActivity.java
+++ app/src/main/java/com/example/user/newcoffeepuzzle/MainActivity.java
@@ -11,6 +11,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -28,18 +31,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private class MyPagerAdapter extends FragmentPagerAdapter {
+
+        List<Page> pageList;
+
         public MyPagerAdapter(FragmentManager fragmentManager) {
             super(fragmentManager);
+            pageList = new ArrayList<>();
+            pageList.add(new Page(new MemFragment(),"一般會員"));
+            pageList.add(new Page(new StoreFragment(),"店家會員"));
         }
 
         @Override
         public Fragment getItem(int position) {
-            return null;
+            return pageList.get(position).getFragment();
         }
 
         @Override
         public int getCount() {
-            return 0;
+            return pageList.size();
         }
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return pageList.get(position).getTitle();
+        }
+
     }
 }
