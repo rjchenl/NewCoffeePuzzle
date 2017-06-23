1bce0bfd81c1deba705dd71145e58ab42b6bc71d
rjchnel <kf135117@gmail.com>
2017-06-23 20:07:03 +0800

    ActivityGetImageTask  done activity_list_fragment.xml   done to be continue ActivityListFragment

---
+++ app/src/main/java/com/example/user/newcoffeepuzzle/activities/ActivityListFragment.java
@@ -0,0 +1,26 @@
+package com.example.user.newcoffeepuzzle.activities;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by user on 2017/6/23.
+ */
+
+public class ActivityListFragment extends Fragment {
+    private static final String TAG = "ActivityListFragment";
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private RecyclerView rvSpots;
+
+
+
+
+
+
+
+
+
+
+
+}
