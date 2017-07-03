2a420df8c0b2c8a504fb86c76958749d5651cf45
rjchnel <kf135117@gmail.com>
2017-07-03 13:27:04 +0800

    show QR code image ok

---
+++ app/src/main/java/com/example/user/newcoffeepuzzle/rjchenl_qrcode/rjchenl_qrcodegen.java
@@ -0,0 +1,12 @@
+package com.example.user.newcoffeepuzzle.rjchenl_qrcode;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by user on 2017/7/2.
+ */
+
+public class rjchenl_qrcodegen extends Fragment {
+
+    
+}
--- app/src/main/java/com/example/user/newcoffeepuzzle/rjchenl_spndcoffeelist/SpndcoffeeListFragment.java
+++ app/src/main/java/com/example/user/newcoffeepuzzle/rjchenl_spndcoffeelist/SpndcoffeeListFragment.java
@@ -1,22 +1,44 @@
 package com.example.user.newcoffeepuzzle.rjchenl_spndcoffeelist;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.user.newcoffeepuzzle.R;
 import com.example.user.newcoffeepuzzle.rjchenl_main.Common_RJ;
 import com.example.user.newcoffeepuzzle.rjchenl_main.Profile;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
+import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by user on 2017/7/1.
@@ -35,6 +57,8 @@ public class SpndcoffeeListFragment extends Fragment {
         View view =inflater.inflate(R.layout.rj_spndcoffeelist_fragment,container,false);
 
         spndList_view = (ListView) view.findViewById(R.id.lvSpndcoffeelist);
+
+
 
         //會用到mem_id 先取得
         Profile profile = new Profile(getContext());
@@ -73,6 +97,7 @@ public class SpndcoffeeListFragment extends Fragment {
     private class SpndCoffeeListAdapter extends BaseAdapter{
         Context context;
         List<SpndcoffeelistVO> spndList_data;
+        Map<ImageView,String> map= new HashMap<>();
 
         public SpndCoffeeListAdapter(Context context, List<SpndcoffeelistVO> spndList_data) {
             this.context = context;
@@ -97,11 +122,30 @@ public class SpndcoffeeListFragment extends Fragment {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
+
             //一開始什麼都還沒按的話  載入被選到的view的實體,也就是convertView
             if(convertView == null){
                 LayoutInflater layoutInflater = LayoutInflater.from(context);
                 convertView =layoutInflater.inflate(R.layout.rj_spndcoffeelist_item_fragment,parent,false);
             }
+
+            //點擊顯示QR_code
+            Button showQRcode = (Button) convertView.findViewById(R.id.showQRcode);
+            showQRcode.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //show QRcode
+//                    Toast.makeText(getActivity(),"顯示QRcode",Toast.LENGTH_SHORT);
+                    AlertDialogFragment alertDialogFragment = new AlertDialogFragment();
+                    FragmentManager fragmentManager = getFragmentManager();
+                    alertDialogFragment.show(fragmentManager,"alert");
+
+
+
+
+
+                }
+            });
 
 
             //取到當下那個view物件
@@ -125,6 +169,92 @@ public class SpndcoffeeListFragment extends Fragment {
 
             return convertView;
         }
+    }
+
+    public static class AlertDialogFragment extends DialogFragment {
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            Dialog dialog = super.onCreateDialog(savedInstanceState);
+            //不顯示標題
+            dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+            return dialog;
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            //載入xml layout
+            View view = inflater.inflate(R.layout.rj_alertdialog, null);
+            return view;
+        }
+
+        @Override
+        public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+
+            //註冊離開button事件聆聽
+            Button btleave = (Button) view.findViewById(R.id.btleave);
+            btleave.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dismiss();
+                }
+            });
+//            放入qr code image
+            ImageView qrimage = (ImageView) view.findViewById(R.id.qrimage);
+            qrimage.setImageResource(R.drawable.default_image);
+
+            //startpaste
+            // QR code 的內容
+            String QRCodeContent = "fuck you men";
+            // QR code 寬度
+            int QRCodeWidth = 800;
+            // QR code 高度
+            int QRCodeHeight = 800;
+            // QR code 內容編碼
+            Map<EncodeHintType, Object> hints = new EnumMap<EncodeHintType, Object>(EncodeHintType.class);
+            hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+
+            MultiFormatWriter writer = new MultiFormatWriter();
+            try
+            {
+
+                String url = "https://www.google.com.tw/";
+                // 容錯率姑且可以將它想像成解析度，分為 4 級：L(7%)，M(15%)，Q(25%)，H(30%)
+                // 設定 QR code 容錯率為 H
+                hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+
+                // 建立 QR code 的資料矩陣
+                BitMatrix result = writer.encode(QRCodeContent, BarcodeFormat.QR_CODE, QRCodeWidth, QRCodeHeight, hints);
+                // ZXing 還可以生成其他形式條碼，如：BarcodeFormat.CODE_39、BarcodeFormat.CODE_93、BarcodeFormat.CODE_128、BarcodeFormat.EAN_8、BarcodeFormat.EAN_13...
+
+                //建立點陣圖
+                Bitmap bitmap = Bitmap.createBitmap(QRCodeWidth, QRCodeHeight, Bitmap.Config.ARGB_8888);
+                // 將 QR code 資料矩陣繪製到點陣圖上
+                for (int y = 0; y<QRCodeHeight; y++)
+                {
+                    for (int x = 0;x<QRCodeWidth; x++)
+                    {
+                        bitmap.setPixel(x, y, result.get(x, y) ? Color.BLACK : Color.WHITE);
+                    }
+                }
+
+                ImageView imgView = (ImageView) view.findViewById(R.id.qrimage);
+                // 設定為 QR code 影像
+                imgView.setImageBitmap(bitmap);
+            }
+            catch (WriterException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            //endpaste
+
+        }
+
+
     }
 
 
