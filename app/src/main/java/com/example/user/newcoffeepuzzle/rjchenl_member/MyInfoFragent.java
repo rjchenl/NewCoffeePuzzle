aff697876f16749e8c1ec229105c0cd022062a00
rjchnel <kf135117@gmail.com>
2017-07-12 16:55:34 +0800

    開始新增會員資料之前

---
+++ app/src/main/java/com/example/user/newcoffeepuzzle/rjchenl_member/MyInfoFragent.java
@@ -0,0 +1,12 @@
+package com.example.user.newcoffeepuzzle.rjchenl_member;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by user on 2017/7/12.
+ */
+
+public class MyInfoFragent extends Fragment {
+
+
+}
